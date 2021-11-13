a8d1685f2b4393b3b29da553840e63e070893d86
Akilan Manivannan <amanivannan@outlook.com>
2021-11-13 16:19:36 -0600

    Stubbed functions + API Endpoints

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,30 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/test")
 async def root():
-    return {"message": "Hello World"}
+    return {"Hello": "World"}
 
+@app.get("/stock/{stock_id}")
+async def get_stock_score(stock_id):
+    '''Function to get a score from all the signals
+    '''
+    # TODO: Get score from signals and aggregate it here
+    return stock_id
+
+
+@app.get("/signal/{signal_id}")
+async def get_signal_reccomendations(signal_id):
+    '''Function to get the top stocks from a given signal
+    '''
+    #TODO: If statements to check for each signal id
+    
+    ret = {
+        "signal_id": signal_id,
+        "stocks": [
+            "APPL",
+            "GOOGLE"
+        ]
+    }
+
+    return ret
