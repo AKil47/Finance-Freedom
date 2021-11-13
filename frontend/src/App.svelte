70b311430229abfd91fb8c550c1c46fe37d9e231
Akilan Manivannan <amanivannan@outlook.com>
2021-11-13 17:16:12 -0600

    Enumerate signals on Front End
    Tested connection between front end and backend and got signal to enumerate based on back end response.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,12 @@
 <script>
+	import Signals from "./Signals.svelte";
+
 	export let name;
 </script>
 
 <main>
 	<p>Hello {name}</p>
+	<Signals></Signals>
 </main>
 
 <style>
---
+++ frontend/src/Signals.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let stocks = []
+
+    onMount(async () => {
+		const res = await fetch("http://localhost:8000/signal", {
+        "method": "GET",
+        "headers": {}
+            });
+		stocks = await res.json();
+	});
+</script>
+
+<ul>
+    {#each stocks as stock, _}
+    <li>{stock}</li>
+    {/each}
+</ul>
+
+<style>
+
+</style>
