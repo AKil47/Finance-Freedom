72f50d931fcd8169b67ae8e60fb3aac17efb00dc
Akilan Manivannan <amanivannan@outlook.com>
2021-11-13 17:59:24 -0600

    Added checkboxes that sync the selected signals to a variable
    Syncs signals to use to chcecked_signals

--- frontend/src/Signals.svelte
+++ frontend/src/Signals.svelte
@@ -1,23 +1,44 @@
 <script>
     import { onMount } from 'svelte';
 
-    let stocks = []
+    let signals = []
+    let signals_checked = []
+    let checked_signals = []
 
     onMount(async () => {
 		const res = await fetch("http://localhost:8000/signal", {
         "method": "GET",
         "headers": {}
             });
-		stocks = await res.json();
+		signals = await res.json();
+        for (let i = 0; i < signals.length; i++) {
+            signals_checked[i] = false
+        }
 	});
+
+    //Send checked boxes to API whenever they change
+    $: {
+        checked_signals = []
+        for (let i = 0; i < signals_checked.length; i++) {
+            if (signals_checked[i]) {
+                checked_signals.push(signals[i])
+            }
+        }
+    }
 </script>
 
-<ul>
-    {#each stocks as stock, _}
-    <li>{stock}</li>
+<div>
+    {#each signals as signal, index}
+    <div class = "signal">
+        <label for="{index}">
+            <input type="checkbox" id="{index}" value="{index}" bind:checked = {signals_checked[index]}> {signal}
+        </label>
+    </div>
     {/each}
-</ul>
+</div>
 
 <style>
-
+    .signal {
+        padding: 1px;
+    }
 </style>
